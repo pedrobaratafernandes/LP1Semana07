bcdd0d63a130d56ae6464353939ab147ff6fb73e
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-04-23 15:51:00 +0100

    4

--- PowerCell/Cell.cs
+++ PowerCell/Cell.cs
@@ -1,27 +1,31 @@
 using System;
+using System.Security.Authentication;
 
 
 namespace PowerCell
 {
     public class Cell
     {
-        private float charge;
+        private float Charge;
         private readonly string Name;
+    
+        private readonly int Level = 1 + (int)Charge/40;
 
 
         public Cell(string name, int charge = 200)
         {
-
+            this.Name = name;
+            this.Charge = charge;
         }
 
         public void Consume(float amount)
         {
-            
+            this.Charge -= amount;
         }
 
         public void Restore()
         {
-            this.charge = 200;
+            this.Charge = 200;
         }
     }
 }
